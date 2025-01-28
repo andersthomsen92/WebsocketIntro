5bbd40607e5dccffbfa9c3b909d7a81c171c12ff
andersthomsen92 <doomknight92@gmail.com>
2025-01-28 22:18:08 +0100

    Initial Commmit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+
+const App = () => {
+    const [messages, setMessages] = useState<string[]>([]);
+    const [messageContent, setMessageContent] = useState<string>(""); 
+    const [ws, setWs] = useState<WebSocket | null>(null); 
+
+    useEffect(() => {
+        const socket = new WebSocket("ws://localhost:8080");
+
+        socket.onmessage = (message) => {
+            setMessages((prevMessages) => [...prevMessages, message.data]);
+        };
+
+        setWs(socket);
+
+        return () => {
+            socket.close();  // Otherwise it sends the message 2 to 4 times
+        };
+    }, []);
+
+
+    const sendMessage = () => {
+            // @ts-ignore
+        ws.send(messageContent);
+            setMessageContent(""); 
+    };
+
+    return (
+        <div>
+            <ul>
+                {messages.map((msg, index) => (
+                    <li key={index}>{msg}</li>
+                ))}
+            </ul>
+
+            <input
+                type="text"
+                value={messageContent}
+                onChange={(e) => setMessageContent(e.target.value)}
+                placeholder="Type a message"
+            />
+            <button onClick={sendMessage}>Send</button>
+        </div>
+    );
+};
+
+export default App;
